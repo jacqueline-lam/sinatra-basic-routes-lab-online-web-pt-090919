cab12d88aeeb2d9c9788f427421718dec8b5f2a4
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-05 23:30:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Jacqueline"
+  end
+  
+  get '/hometown' do
+    "My hometown is Hong Kong"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'You Were Right' by Rufus du Sol."
+  end
 end
